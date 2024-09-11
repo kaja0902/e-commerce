bce5f9c3aaed1c4574ee87f6e484ed09c07b109a
Katarina Antic <kaja@MacBook-Pro-85.local>
2024-09-11 13:17:13 +0200

    bug fix na cmd-u

--- resources/views/admin/users/view.blade.php
+++ resources/views/admin/users/view.blade.php
@@ -15,43 +15,43 @@
                     <div class="row">
                         <div class="col-md-4">
                         <label for="">Role</label>
-                            <div class="p-2 border">{{ $users->role_as == '0' ? 'User':'Admin' }}</div>
+                            <div class="p-2 border">{{ $user->role_as == '0' ? 'User':'Admin' }}</div>
                         </div>
                         <div class="col-md-4">
                             <label for="">First Name</label>
-                            <div class="p-2 border">{{ $users->name }}</div>
+                            <div class="p-2 border">{{ $user->name }}</div>
                         </div>
                         <div class="col-md-4">
                             <label for="">Last Name</label>
-                            <div class="p-2 border">{{ $users->lname }}</div>
+                            <div class="p-2 border">{{ $user->lname }}</div>
                         </div>
                         <div class="col-md-4">
                             <label for="">Email</label>
-                            <div class="p-2 border">{{ $users->email }}</div>
+                            <div class="p-2 border">{{ $user->email }}</div>
                         </div>
                         <div class="col-md-4">
                             <label for="">Phone</label>
-                            <div class="p-2 border">{{ $users->phone }}</div>
+                            <div class="p-2 border">{{ $user->phone }}</div>
                         </div>
                         <div class="col-md-4">
                             <label for="">Adress 1</label>
-                            <div class="p-2 border">{{ $users->adress1 }}</div>
+                            <div class="p-2 border">{{ $user->adress1 }}</div>
                         </div>
                         <div class="col-md-4">
                             <label for="">Adress 2</label>
-                            <div class="p-2 border">{{ $users->adress2 }}</div>
+                            <div class="p-2 border">{{ $user->adress2 }}</div>
                         </div>
                         <div class="col-md-4">
                             <label for="">City</label>
-                            <div class="p-2 border">{{ $users->city }}</div>
+                            <div class="p-2 border">{{ $user->city }}</div>
                         </div>
                         <div class="col-md-4">
                             <label for="">Country</label>
-                            <div class="p-2 border">{{ $users->country }}</div>
+                            <div class="p-2 border">{{ $user->country }}</div>
                         </div>
                         <div class="col-md-4">
                             <label for="">Zip code</label>
-                            <div class="p-2 border">{{ $users->zipcode }}</div>
+                            <div class="p-2 border">{{ $user->zipcode }}</div>
                         </div>
                     </div>
                 </div>
